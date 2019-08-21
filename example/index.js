a5bd48878fd5529bd828ebad32ff93eec92a27cd
websoptimization <web-optimization@GRESI.local>
2019-08-21 18:55:13 +0530

    solving error of adding example

--- example/index.js
+++ example/index.js
@@ -2,7 +2,7 @@
  * @format
  */
 
-//import {AppRegistry} from 'react-native';
+import {AppRegistry} from 'react-native';
 import App from './App';
 
-//AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('DashedProgress', () => App);
